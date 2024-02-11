6b231b7c66155594bdd5446e6571c32c0fe8d36e
strider73 <strider.lee@gmail.com>
2024-02-12 00:47:53 +1100

    Login module update for other SNS 1. create new property loginSource in  UserModel 2. loginSource will be set     *  when user signIn     * or  userdata has been recovered from UserDeault Object

---
+++ AdventureTube/Models/User/LoginSource.swift
@@ -0,0 +1,14 @@
+//
+//  LoginSource.swift
+//  AdventureTube
+//
+//  Created by chris Lee on 8/2/2024.
+//
+
+enum LoginSource : String ,Codable {
+    case facebook
+    case google
+    case apple
+    case instagram
+    case twitter
+}
