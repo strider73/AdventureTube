abcac1203cc560459d552f5b6f51a392781585c4
strider73 <strider.lee@gmail.com>
2024-07-05 23:50:02 +1000

    Add userData property , it will be reference for LoginManager.useData since all API call need a useData info , for example token or adventuretube_id

--- AdventureTube/Services/APIService/Adventuretube/AdventureTubeAPIService.swift
+++ AdventureTube/Services/APIService/Adventuretube/AdventureTubeAPIService.swift
@@ -3,6 +3,11 @@
 //  AdventureTube
 //
 //  Created by chris Lee on 29/11/2023.
+//  API Service  will be used by user
+//  with adventuretube_id, refresh_token , access_token ....
+//  so its crutial that user data has been set and update accordingly
+//  in order to do that userdata will come from LoginManager.userData
+
 //https://medium.com/@hemalasanka/making-api-calls-with-ios-combines-future-publisher-7a5011f81c2
 
 import Foundation
@@ -10,16 +15,22 @@ import UIKit
 import Combine
 
 class AdventureTubeAPIService : NSObject , AdventureTubeAPIPrototol {
-
+    
     
     //This is SingleTon Parttern
     static let shared = AdventureTubeAPIService()
+    var userData : UserModel?
     private var cancellables = Set<AnyCancellable>() // (3)
-    private var accessToken : String?
-    private var refreshToken : String?
+    //    private var accessToken : String?
+    //    private var refreshToken : String?
     private  var targetServerAddress: String = "http://192.168.1.106:8030"
     //private  var targetServerAddress: String = "https://api.adventuretube.net"
     
+    
+    //    private init( userData :UserModel){
+    //        self.userData = userData
+    //    }
+    //
     
     func getData<T: Decodable>(endpoint: String, id: Int? = nil, returnData: T.Type) -> Future<T, Error> {
         return Future<T, Error> { [weak self] promise in  // (4) -> Future Publisher
@@ -64,52 +75,54 @@ class AdventureTubeAPIService : NSObject , AdventureTubeAPIPrototol {
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         // Safely unwrap optional properties
-           guard let idToken = adventureUser.idToken,
-                 let fullName = adventureUser.fullName,
-                 let emailAddress = adventureUser.emailAddress,
-                 let password = adventureUser.googleUserId
-            else {
-               fatalError("Missing user information")
-           }
+        guard let idToken = adventureUser.idToken,
+              let fullName = adventureUser.fullName,
+              let emailAddress = adventureUser.emailAddress,
+              let password = adventureUser.googleUserId
+        else {
+            fatalError("Missing user information")
+        }
         let body: [String: Any] = [
-                "googleIdToken": idToken,
-                "username": fullName,
-                "email": emailAddress,
-                "role": "USER",
-                "password": "1111111"
-            ]
+            "googleIdToken": idToken,
+            "username": fullName,
+            "email": emailAddress,
+            "role": "USER",
+            "password": "1111111"
+        ]
         request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
         print("googleIdToken : \(idToken)")
         return URLSession.shared.dataTaskPublisher(for: request)
-                .tryMap { result -> Data in
-                    guard let httpResponse = result.response as? HTTPURLResponse else {
-                        throw BackendError.unknownError
-                    }
-
-                    if (200...299).contains(httpResponse.statusCode) {
-                        return result.data
-                    } else {
-                        let errorResponse = try? JSONDecoder().decode(AuthResponse.self, from: result.data)
-                        let errorMessage = errorResponse?.errorMessage ?? "Unknown server error"
-                        throw BackendError.serverError(message: errorMessage)
-                    }
+            .tryMap { result -> Data in
+                guard let httpResponse = result.response as? HTTPURLResponse else {
+                    throw BackendError.unknownError
                 }
-                .decode(type: AuthResponse.self, decoder: JSONDecoder())
-                .mapError { error -> BackendError in
-                    if let backendError = error as? BackendError {
-                        return backendError
-                    } else if let decodingError = error as? DecodingError {
-                        return BackendError.decodingError(message: decodingError.localizedDescription)
-                    } else {
-                        return BackendError.unknownError
-                    }
+                
+                if (200...299).contains(httpResponse.statusCode) {
+                    return result.data
+                } else {
+                    let errorResponse = try? JSONDecoder().decode(AuthResponse.self, from: result.data)
+                    let errorMessage = errorResponse?.errorMessage ?? "Unknown server error"
+                    throw BackendError.serverError(message: errorMessage)
                 }
-                .receive(on: DispatchQueue.main)
-                .handleEvents(receiveOutput: { [weak self] authResponse in
-                           self?.accessToken = authResponse.accessToken
-                           self?.refreshToken = authResponse.refreshToken
-                 })
-                .eraseToAnyPublisher()
+            }
+            .decode(type: AuthResponse.self, decoder: JSONDecoder())
+            .mapError { error -> BackendError in
+                if let backendError = error as? BackendError {
+                    return backendError
+                } else if let decodingError = error as? DecodingError {
+                    return BackendError.decodingError(message: decodingError.localizedDescription)
+                } else {
+                    return BackendError.unknownError
+                }
+            }
+            .receive(on: DispatchQueue.main)
+            .handleEvents(receiveOutput: { [weak self] authResponse in
+                guard let self = self else {return}
+                self.userData?.adventuretubeJWTToken = authResponse.accessToken
+                self.userData?.adventuretubeRefreshJWTToken = authResponse.refreshToken
+                self.userData?.adventureTube_id = authResponse.userDetails?.id
+            })
+            .eraseToAnyPublisher()
     }
     
     func signOut() -> AnyPublisher<RestAPIResponse, Error> {
@@ -117,58 +130,58 @@ class AdventureTubeAPIService : NSObject , AdventureTubeAPIPrototol {
             fatalError("Invalid URL")
         }
         
-        guard let refreshToken = refreshToken else {
-               return Fail(error: NetworkError.invalidURL)
-                   .eraseToAnyPublisher()
+        guard let refreshToken = userData?.adventuretubeRefreshJWTToken else {
+            return Fail(error: NetworkError.invalidURL)
+                .eraseToAnyPublisher()
         }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue(refreshToken, forHTTPHeaderField: "Authorization")
         print("Sending signOut request to \(url.absoluteString) with refreshToken: \(refreshToken)")
-
+        
         return URLSession.shared.dataTaskPublisher(for: request)
-                .tryMap { result -> Data in
-                    guard let httpResponse = result.response as? HTTPURLResponse else {
-                        throw BackendError.unknownError
-                    }
-
-                    if (200...299).contains(httpResponse.statusCode) {
-                        return result.data
-                    } else {
-                        let errorResponse = try? JSONDecoder().decode(AuthResponse.self, from: result.data)
-                        let errorMessage = errorResponse?.errorMessage ?? "Unknown server error"
-                        throw BackendError.serverError(message: errorMessage)
-                    }
+            .tryMap { result -> Data in
+                guard let httpResponse = result.response as? HTTPURLResponse else {
+                    throw BackendError.unknownError
                 }
-                .decode(type: RestAPIResponse.self, decoder: JSONDecoder())
-                .mapError { error -> BackendError in
-                    if let backendError = error as? BackendError {
-                        return backendError
-                    } else if let decodingError = error as? DecodingError {
-                        return BackendError.decodingError(message: decodingError.localizedDescription)
-                    } else {
-                        return BackendError.unknownError
-                    }
+                
+                if (200...299).contains(httpResponse.statusCode) {
+                    return result.data
+                } else {
+                    let errorResponse = try? JSONDecoder().decode(AuthResponse.self, from: result.data)
+                    let errorMessage = errorResponse?.errorMessage ?? "Unknown server error"
+                    throw BackendError.serverError(message: errorMessage)
                 }
-                .receive(on: DispatchQueue.main)
-                .eraseToAnyPublisher()
-
+            }
+            .decode(type: RestAPIResponse.self, decoder: JSONDecoder())
+            .mapError { error -> BackendError in
+                if let backendError = error as? BackendError {
+                    return backendError
+                } else if let decodingError = error as? DecodingError {
+                    return BackendError.decodingError(message: decodingError.localizedDescription)
+                } else {
+                    return BackendError.unknownError
+                }
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+        
     }
 }
 enum BackendError: LocalizedError {
     case serverError(message: String)
     case decodingError(message: String)
     case unknownError
-
+    
     var errorDescription: String? {
         switch self {
-        case .serverError(let message):
-            return message
-        case .decodingError(let message):
-            return message
-        case .unknownError:
-            return "An unknown error occurred"
+            case .serverError(let message):
+                return message
+            case .decodingError(let message):
+                return message
+            case .unknownError:
+                return "An unknown error occurred"
         }
     }
 }
